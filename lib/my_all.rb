47b1a5e268f29e5a8435844767105180b169572f
Liz Burton <liz.burton147@gmail.com>
2017-11-23 18:17:41 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,12 +4,7 @@ def my_all?(collection)
   i = 0
   while i < collection.length
     binding.pry
-    # if !yield(collection[i])
     yield(collection[i])
-    #   return false
-    # end
     i += 1
   end
-  # true
-  # binding.pry
 end
