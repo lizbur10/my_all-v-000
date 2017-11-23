e95cdd4a4cbd0169981250d086d9da8b1162d066
Liz Burton <liz.burton147@gmail.com>
2017-11-23 18:03:09 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,12 +3,13 @@ require 'pry'
 def my_all?(collection)
   i = 0
   while i < collection.length
-    binding.pry
-    if !yield(collection[i])
-      return false
-    end
+    # binding.pry
+    # if !yield(collection[i])
+    yield(collection[i])
+    #   return false
+    # end
     i += 1
   end
-  true
-  binding.pry
+  # true
+  # binding.pry
 end
