d1290c157f9b4558949c56c10c6999c8ada04785
Liz Burton <liz.burton147@gmail.com>
2017-11-23 17:57:04 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  while i < collection.size
+    if !yield(collection[i])
+      return false
+    end
+    i += 1
+  end
+  true
+  binding.pry
 end
+
+my_all?([1,2,3]) {|i| i < 2}
